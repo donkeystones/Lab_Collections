238d9fcd35d7eef3b3a40257cd9a42598dc50a61
preyerguilt <oscar.back96@outlook.com>
2019-05-12 21:26:48 +0200

    djurinventering part 2 update

--- src/Lab_Collections_Part_2/Djurinventering_part2.java
+++ src/Lab_Collections_Part_2/Djurinventering_part2.java
@@ -5,10 +5,37 @@
  */
 package Lab_Collections_Part_2;
 
+import Lab_Collections_Part_1.AnimalInventory;
+import Lab_Collections_Part_1.AnimalReport;
+import java.io.IOException;
+import java.util.*;
+
 /**
  *
  * @author Oscar Bäck
  */
 public class Djurinventering_part2 {
+    
+    
+    private static AnimalInventory AI;
+
+    public Djurinventering_part2() throws IOException {
+        
+    }
+    
+    public static void main(String[]args) throws IOException{
+        TreeMap tm = new TreeMap();
+        AI = new AnimalInventory();
+        
+        AI.getReports().forEach((rep) -> {
+            if(tm.containsKey(rep.getArt())){
+                tm.put(rep.getArt(),rep.getAmount()+(int)tm.get(rep.getArt()));
+            }else{
+                tm.put(rep.getArt(), rep.getAmount());
+            }
+        });
+        
+        System.out.println(tm.get("Lejon"));
+    }
     
 }
